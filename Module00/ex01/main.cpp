c1f67bc56000c8f11e65960ffe0ef193fcb2e580
Kiss Gautier <gkiss00>
2020-11-24 22:06:24 +0100

    etrange

--- Module00/ex01/main.cpp
+++ Module00/ex01/main.cpp
@@ -1,15 +1,26 @@
 #include "phonebook.hpp"
 
+static std::string trim(std::string str){
+    int     start;
+    int     end;
+    
+    start = str.find_first_not_of(" ");
+    end = str.find_last_not_of(" ");
+    str.erase(end + 1);
+    str.erase(0, start);
+    return (str);
+}
+
 static int checkIndex(PhoneBook *phoneBook, int index)
 {
     if (index < 1 || index > 8)
     {
-        std::cout << "Index hors interval\n";
+        std::cout << "Index hors interval" << std::endl;
         return (-1);
     }
     if (phoneBook[0].getActivated(index - 1) == 0)
     {
-        std::cout << "Il n y a personne a cet index\n";
+        std::cout << "Il n y a personne a cet index" << std::endl;
         return (-1);
     }
     return (index);
@@ -26,7 +37,7 @@ static void searchContact(PhoneBook *phoneBook)
             std::cout << "index : ";
             while ( ! ( std::cin >> index ) ) 
             { 
-                std::cerr << "Erreur de saisie.\n";
+                std::cout << "Erreur de saisie" << std::endl;
                 std::cout << "index : ";
                 std::cin.clear(); 
                 std::cin.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
@@ -36,7 +47,7 @@ static void searchContact(PhoneBook *phoneBook)
     }
     else
     {
-        std::cout << "votre annuaire est vide\n";
+        std::cout << "votre annuaire est vide" << std::endl;
     }
     
 }
@@ -50,34 +61,32 @@ static void addContact(PhoneBook *phoneBook)
     i = 0;
     while (phoneBook[0].getActivated(i) != 0)
         ++i;
-    if (i == 8)
+    if (i == 1)
     {
         std::cout << "votre annuaire telephonique est plein, voulez vous supprimez qql ?";
         do{
             std::cout << "(Y or N) : ";
-            std::cin >> tmp;
-            std::cin.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
+            getline(std::cin, tmp);
+            tmp = trim(tmp);
         }while (tmp.compare("Y") != 0 && tmp.compare("N") != 0);
         if (tmp.compare("N") == 0)
             return ;
         else
         {
-            std::cout << "Entrez l index de la personne que vous souhaiter supprimer\n";
+            std::cout << "Entrez l index de la personne que vous souhaiter supprimer" << std::endl;
             do{
                 std::cout << "index : ";
                 while ( ! ( std::cin >> index ) ) 
-                { 
-                    std::cerr << "Erreur de saisie.\n";
+                {
+                    std::cout << "Erreur de saisie" << std::endl;
                     std::cout << "index : ";
-                    std::cin.clear(); 
-                    std::cin.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
                 }
             }while (checkIndex(phoneBook, index) == -1);
             phoneBook[0].deleteContact(index - 1);
             i = index - 1;
         }    
     }
-    std::cout << "Enregistrement de votre nouveau contact\n";
+    std::cout << "Enregistrement de votre nouveau contact" << std::endl;
     phoneBook[0].getContacts()[i].setFirstName();
     phoneBook[0].getContacts()[i].setLastName();
     phoneBook[0].getContacts()[i].setNickName();
@@ -96,6 +105,7 @@ int     main()
 {
     PhoneBook *phoneBook = new PhoneBook();
     std::string cmd;
+    std::string tmp;
 
     while (true)
     {
@@ -105,9 +115,9 @@ int     main()
             ADD, pour ajouter un contact\n\
             SEARCH, pour rechercher un contact\n\
             EXIT, pour sortir\n";
-            std::cin >> cmd;
+            getline(std::cin, tmp);
+            cmd = trim(tmp);
         } while (cmd.compare("ADD") != 0 && cmd.compare("SEARCH") != 0 && cmd.compare("EXIT"));
-        std::cin.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
         if (cmd.compare("ADD") == 0)
             addContact(phoneBook);
         else if (cmd.compare("SEARCH") == 0)
@@ -117,5 +127,5 @@ int     main()
 
     }
     delete(phoneBook);
-    return (1);
+    return (0);
 }
