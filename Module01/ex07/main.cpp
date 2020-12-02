9c6852debac98bda93b10152f5c27b3b5ee89076
Kiss Gautier <gkiss00>
2020-12-02 13:08:49 +0100

    oui

--- Module01/ex07/main.cpp
+++ Module01/ex07/main.cpp
@@ -30,33 +30,25 @@ int check_error(int argc, char **argv)
     return (-1);
 }
 
-int replace_word(std::string &original, const std::string &aTrouver, const std::string &aRemplacer)
+std::string replace_word(std::string original, const std::string aTrouver, const std::string aRemplacer)
 {
-    int n = original.find(aTrouver);
-    if (n==std::string::npos)
-    { 
-        return 0;
-    }
-    else
-    {
-        original.replace(n,aTrouver.size(),aRemplacer);
-        return 1+replace_word(original, aTrouver, aRemplacer);
-    }
+    std::string new_content;
+
+    new_content = "";
+    for (int i=0; i < original.size(); i++)
+	{
+		if (!(original.substr(i)).find(aTrouver))
+		{
+			i += aTrouver.size() - 1;
+			new_content += aRemplacer;
+		}
+		else
+			new_content += original[i];
+	}
+    return (new_content);
 }
 
-std::string get_contenu(std::string contenu)
-{
-    std::string content;
-
-    content = "";
-    for (int i = 0; i < contenu.length() - 1; ++i)
-    {
-        content += contenu.at(i);
-    }
-    return (content);
-}
-
-int copy_in_new_file(char *name, std::string content)
+int copy_in_new_file(char *name, std::string new_content)
 {
     std::string extension = ".replace";
     std::string file_name = name + extension;
@@ -75,7 +67,7 @@ int copy_in_new_file(char *name, std::string content)
         std::cout << "Probleme lors de l ouverture du fichier de copie" << std::endl;
         return (-1);
     }
-    file << content << std::endl;
+    file << new_content;
     file.close();
     return (0);
 }
@@ -84,6 +76,7 @@ int copy_file(char **argv)
 {
     std::string line;
     std::string content;
+    std::string new_content;
     std::ifstream file(argv[1]);
 
     content = "";
@@ -97,10 +90,12 @@ int copy_file(char **argv)
         content += line;
         content += "\n";
     }
-    content = get_contenu(content);
+    content.erase(content.size() - 1);
     file.close();
-    replace_word(content, argv[2], argv[3]);
-    if (copy_in_new_file(argv[1], content) == -1)
+    std::cout << content << std::endl;
+    new_content = replace_word(content, argv[2], argv[3]);
+    std::cout << new_content << std::endl;
+    if (copy_in_new_file(argv[1], new_content) == -1)
         return (-1);
     return (1);
 }
@@ -108,8 +103,8 @@ int copy_file(char **argv)
 int main(int argc, char **argv)
 {
     if (check_error(argc, argv) == -1)
-        return (0);
+        return (1);
     if (copy_file(argv) == -1)
-        return (0);
-    return (1);
+        return (1);
+    return (0);
 }
